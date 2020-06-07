9fcc53a5f76899b7b68dbdcaab458b2713875bcc
IvanNess <chai@bk.ru>
2020-06-07 12:27:31 +0300

    one more try to github pages

---
+++ build/precache-manifest.d96ec5dcf48e5c8cc370bee5de75cbe6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4127a630033cdac97fc1a5fff3052fd3",
+    "url": "/impress/index.html"
+  },
+  {
+    "revision": "ddbbec7303f8e2cd4a81",
+    "url": "/impress/static/css/main.bed3475f.chunk.css"
+  },
+  {
+    "revision": "d4ddc849988a42b51e45",
+    "url": "/impress/static/js/2.0b391f30.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/impress/static/js/2.0b391f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddbbec7303f8e2cd4a81",
+    "url": "/impress/static/js/main.3288febb.chunk.js"
+  },
+  {
+    "revision": "c9bd94a898cbfa4010e4",
+    "url": "/impress/static/js/runtime-main.182fd135.js"
+  },
+  {
+    "revision": "7212787ac2cb006948236e4600392180",
+    "url": "/impress/static/media/EBGaramond-Regular.7212787a.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/impress/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "9574c6cfe5448e84a7eebf6d68022e4d",
+    "url": "/impress/static/media/all.9574c6cf.jpg"
+  }
+]);
