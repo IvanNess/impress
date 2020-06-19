e8cb1057008edf4713279ad67366f4ef3914b400
IvanNess <chai@bk.ru>
2020-06-19 19:45:58 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/impress/precache-manifest.79861e784a4b2b6abb552a8b13aab654.js"
+  "/impress/precache-manifest.851e77458e87d3365aa00b2f4e12aac7.js"
 );
 
 self.addEventListener('message', (event) => {
